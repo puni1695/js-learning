e2d6ed9c8b007f58567a93aba9234fa1f1661c5b
puni1695 <puni1695@gmail.com>
2025-12-08 17:30:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "puneet@gmail.com"
+var accountPwd = "12345" 
+accountCity = "Jaipur"   //not a good practice
+
+// accountId = 144552   not allowed 
+
+accountEmail = "pks@gmail.com"
+
+var accountPwd = "1234"
+
+accountCity = "Aligarh" 
+
+let accountState
+
+console.log(accountId);
+/*
+prefer nt to use var bcause of issue in 
+*/
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";     // treat all JS code as newer version
+
+//alert(3+3)  // we r using node.js not browser
+
+console.log(3+3);    // code readability should be high
+console.log("Puneet")
+
+
+let name = "Puneet"     //string => ""
+let age = 28            //number => 2^53  if number is even bigger => bigint
+let isLoggedIn = true   //boolean => true/false
+                        //null   => standalone value
+                        //undefined =>  value not assigned yet
+                        //symbol => used for uniqueness
+
+
+                        //object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
+
