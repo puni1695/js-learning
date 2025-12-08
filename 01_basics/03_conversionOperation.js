1c5ad3af6eab67b02844ec5769bcfca7a941cdfb
puni1695 <puni1695@gmail.com>
2025-12-08 17:47:33 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = false
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1
+
+
+let isLoggedIn = "null"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//  null => false
+//  0 => false
+//  "" => false
+//  "null" => true
+
+let num = 33
+
+let stringNum = String(num);
+console.log(stringNum)
+console.log(typeof stringNum)
+
