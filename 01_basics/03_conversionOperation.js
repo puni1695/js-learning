35760bfb2aa8632d964163dabc82cb531a789690
puni1695 <puni1695@gmail.com>
2025-12-08 18:10:14 +0000

    operations of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let num = 33
 
 let stringNum = String(num);
-console.log(stringNum)
-console.log(typeof stringNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
 
+
+//          OPERATIONS          //
+
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue)
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2+2);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = " Puneet"
+// console.log(str1 + str2);
+let str3 = str1+ str2;
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+console.log(+true); //1
+console.log(+"");   //0
+
+let num1, num2, num3
+
+num1 = num2 = num3
+
+let count  =100
+++count
+console.log(--count);
