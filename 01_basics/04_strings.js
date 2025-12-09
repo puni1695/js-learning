edd8fcd0c6b63beb1b1a414c91b9fc5fe854c819
puni1695 <puni1695@gmail.com>
2025-12-09 19:37:38 +0000

    string methods

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,32 @@
+const name = "Puneet"
+const repoCOunt = 5
+
+// console.log(name + repoCOunt + " Value");
+
+// console.log(`Hi! My name is ${name} and my repo count is ${repoCOunt}`);
+
+const gameName = new String('puneet')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('t'));
+// console.log(gameName.substring(0, 4));
+
+const s1 = gameName.slice(-3, 4)
+// console.log(s1);
+
+const newStr = "   Puneet Singh   "
+// console.log(newStr.trim());
+
+const newStr1 = "Puneet,Kumar,Singh"
+// console.log(newStr1.replaceAll(',', ' '));
+const newStr2 = "Puneet Kumar Singh"
+// console.log(newStr2.includes('Ka'));
+// console.log(newStr1.split(','));
+// console.log(newStr.concat(newStr1))
+console.log(newStr2.slice(7, 13));
+console.log(newStr2.slice(-5));
+
