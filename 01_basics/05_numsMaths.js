4156cc07e772712bc228fe31552519e8ea64e1c3
puni1695 <puni1695@gmail.com>
2025-12-10 06:43:44 +0000

    math and numbers

---
+++ 01_basics/05_numsMaths.js
@@ -0,0 +1,24 @@
+const bal = new Number(123.8866);
+// console.log(bal);
+
+// console.log(bal.toString());
+// console.log(bal.toFixed(2));
+// console.log(bal.toPrecision(2));
+
+// console.log(bal.toFixed(1));
+
+const num = 1000000
+// console.log(num.toLocaleString('en-IN'));
+
+//          MATHS           //
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(7.35));
+// console.log(Math.min(4,2,5,1));
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
